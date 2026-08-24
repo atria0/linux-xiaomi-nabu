73015930965b4504c1e471b5368c8ff22d0ccdf7
Seimo <43766162+SeimoDev@users.noreply.github.com>
2026-08-24 11:46:31 +0800

    gpu: import Qualcomm KGSL as an out-of-tree module for nabu
    Import kgsl @ 72383caaef29830f77c5314945cddcd293d817f2 into
    drivers/gpu/msm-kgsl and keep the nabu IOMMU probe/abort fixes.
    Build with M=drivers/gpu/msm-kgsl.

---
+++ drivers/gpu/msm-kgsl/downstream_dependencies/linux/clk/qcom.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef __LINUX_CLK_QCOM_H_
+#define __LINUX_CLK_QCOM_H_
+
+#include <linux/clk.h>
+#include <linux/regulator/consumer.h>
+
+#endif  /* __LINUX_CLK_QCOM_H_ */
